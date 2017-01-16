695737a76f3f35d02492791248f545d8a8ccd92a
roadtodroid <roadtodroid@gmail.com>
2017-01-16 05:07:08 -0500

    Premium payment first commit

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -1,19 +1,3 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -25,6 +9,16 @@ import (
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
+}
+
+type PremiumPayment struct {
+	PolicyNumber string `json:"policynumber"`
+	DOB int64 `json:"dob"`				//utc date
+	Email string `json:"email"`
+	ContactNumber string `json:"contactnumber"`
+	Name string `json:"name"`
+	DueDate int64 `json:"duedate"` 	//utc date
+	Amount int64 `json:"amount"`
 }
 
 func main() {
