4fd9668001a414c530134a9c7102de97e81c9b3d
roadtodroid <roadtodroid@gmail.com>
2017-01-16 12:48:28 -0500

    fix claim insurance TODO: check if again claimed

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -430,7 +430,7 @@ func (t *SimpleChaincode) claim_insurance(stub shim.ChaincodeStubInterface, args
 	}
 
 	//Create a claim object
-	var claim = ClaimInsurance{ AccID: id ,PolicyNumber: policynumber, TypeOfClaim: insuranceType, DocVerified: true, Amount: amount}
+	var claim = ClaimInsurance{ AccID: id ,PolicyNumber: policynumber, TypeOfClaim: insuranceType, DocVerified: docverified, Amount: amount}
  	claimBytes, err := json.Marshal(&claim)
   if err != nil {
 		fmt.Println("error claiming insurance for " + id +" and policy no - " + policyNumberString )
